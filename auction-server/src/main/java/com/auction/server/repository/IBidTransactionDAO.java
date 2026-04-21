5491a098b2e3a5910c7588964456655a4b4a2c01
Khan <khanhnamdmx@gmail.com>
2026-04-22 03:02:01 +0700

    MOREE DAO update (waiting for bid constructor fixing)

---
+++ auction-server/src/main/java/com/auction/server/repository/IBidTransactionDAO.java
@@ -0,0 +1,15 @@
+package com.auction.server.repository;
+
+import com.auction.server.model.BidTransaction;
+import java.util.List;
+
+public interface IBidTransactionDAO {
+    // Lưu 1 lần đặt giá xuống DB
+    boolean insertBid(BidTransaction transaction);
+
+    // Lấy toàn bộ lịch sử đặt giá của 1 phiên (dùng để vẽ biểu đồ)
+    List<BidTransaction> getBidsByAuctionId(int auctionId);
+
+    // Lấy giá cao nhất hiện tại của 1 phiên
+    BidTransaction getHighestBid(int auctionId);
+}
